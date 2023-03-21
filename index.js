17ebf1629d038c926c47674e07633ebd22d33f8c
Rahultej05N <rahultejnedunuri@gmail.com>
2023-03-21 23:26:08 +0530

    cashregis

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-const billAmount = document.querySelector("#bill-amount");
+
 const checkButton = document.querySelector("#check-button");
-const givenCash = document.querySelector("#given-cash");
+
 const message = document.querySelector("#error-message");
 const notes = document.querySelectorAll(".no-of-notes");
-const notes1 = document.querySelectorAll(".no-of-notes1");
+const notes1=document.querySelectorAll(".no-of-notes1");
 const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
-let input = document.querySelector("#given-cash");
-let button = document.querySelector("#check-button");
 
+const numberInput = document.getElementById('given-cash');
+const submitButton = document.getElementById('check-button');
 
+numberInput.addEventListener('input', function() {
+  if (numberInput.value === '') {
+    submitButton.disabled = true;
+  } else {
+    
+    submitButton.disabled = false;
+  }
+});
 
-button.disabled = true;
-
-input.addEventListener("change", stateHandle);
-
-function stateHandle() {
-	if (document.querySelector(".input").value === "") {
-		button.disabled = true;
-	} else {
-		button.disabled = false;
-	}
-}
 
 
 checkButton.addEventListener("click",
     function validateBillAndGivenCash() {
+        var billAmount = Number(document.querySelector("#bill-amount").value);
+        var givenCash = Number(document.querySelector("#given-cash").value);
+        
         hideMessage();
-        if (billAmount.value > 0) {
-            if (givenCash.value >= billAmount.value) {
-                const amountToBeReturnAmount = givenCash.value - billAmount.value;
-                console.log(amountToBeReturnAmount)
+        if (billAmount > 0) {
+            if (givenCash >= billAmount) {
+                const amountToBeReturnAmount = givenCash - billAmount;
+                const amount=billAmount;
                 calculateNotes(amountToBeReturnAmount);
+                calculate(amount);
             } 
             
-            else if(givenCash.value < billAmount.value){
-                showMsg("Check the bill Sir!")
+            else if(givenCash < billAmount){
+                showMsg("Given cash is less than the Bill Amount!")
             }
         } else {
             showMsg("Enter Validate Bill Amount")
@@ -50,6 +51,13 @@ function calculateNotes(amountToBeReturnAmount) {
     }
 }
 
+function calculate(amount) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const noOfNotes = Math.trunc( amount/ availableNotes[i]);
+        amount = amount % availableNotes[i];
+        notes1[i].innerText = noOfNotes
+    }
+}
 
 function hideMessage() {
     message.style.display = "none";
